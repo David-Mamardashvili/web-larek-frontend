822fe559096ab88229364f783fbf3e45f8c1cdd5
David-Mamardashvili <144165614+David-Mamardashvili@users.noreply.github.com>
2024-05-08 01:32:10 +0300

    second commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,51 +1,27 @@
-import { IOrder } from './index';
+export interface IProduct { 
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+}
 
-export interface IProductItem {
-    id: string;
-    title: string;
-    description: string;
-    image: string;
-    category: string;
-    price: number;
-  }
+export interface IOrder {
+  payment: 'online' | 'upon receipt';
+  email: string;
+  phone: string;
+  address: string;
+  total: number;
+  items: string[];
+}
 
-  export type IProducts = IProductItem[];
-  
-  export interface IProductList {
-    total: number;
-    items: IProducts;
-  }
-  
-  export interface ICartList {
-    total: number;
-    items: IProducts;
-    summary: number;
-  }
+export type IProductList = Omit<IProduct[], 'description'>;
 
-  export type IPayment = 'online' | 'receipt'
-  
-  export interface IOrderForm {
-    payment: IPayment;
-    email: string;
-    phone: string;
-    address: string;
-  }
+export type IOrderBasket = Pick<IProduct, 'description' | 'price'> & Pick<IOrder, 'total'>
 
-  export interface IOrder extends IOrderForm {
-    total: number;
-    items: string[];
-    isPaid: boolean;
-  }
-  
-  export type FormErrors = Partial<Record<keyof IOrder, string>>;
-  
-  export interface IOrderResult {
-    id: string;
-  }
-  
-  export interface IAppState {
-    catalog: IProductList[];
-    cart: ICartList[];
-    order: IOrder | null;
-    loading: boolean;
-  }
+export type IOrderPayment = Pick<IOrder, 'payment' | 'address'>
+
+export type IOrderContacts = Pick<IOrder, 'email' | 'phone'>
+
+export type IOrderResult = Pick<IOrder, 'total'>
